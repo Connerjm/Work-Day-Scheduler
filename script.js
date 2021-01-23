02924d2b1a8025db9cc51c69f74698de0a4849bf
Conner Martin <connerjm2@gmail.com>
2021-01-22 17:01:15 -0800

    Added comments to the JavaScript file.

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-var DateTime = luxon.DateTime;
+//Variables
 
-function testingtime ()
+var DateTime = luxon.DateTime;//The DateTime object that we get from Luxon.
+
+//Main functions
+
+function initialize()//Sets up everything when the application is launched.
+{
+    setDate();
+}
+
+//Helper functions
+
+function setDate ()//Sets the current date in the jumbotron.
 {
     $("#currentDay").text(DateTime.local().toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY));
 }
 
-testingtime();
+//Listeners
+
+//Calling functions
+
+initialize();//Get the ball rolling.
