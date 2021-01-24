fcc7907a9a07e08dfc0b86c644415162d58b5d98
Conner Martin <connerjm2@gmail.com>
2021-01-23 21:16:35 -0800

    Added a function to create shells of the time blocks.

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
 //Variables
 
-var DateTime = luxon.DateTime;//The DateTime object that we get from Luxon.
+var dt = luxon.DateTime.local();//The DateTime object that we get from Luxon.
+var currentHour;
 
 //Main functions
 
 function initialize()//Sets up everything when the application is launched.
 {
+    currentHour = dt.hour;
     setDate();
+    //Build 12ish time blocks
+    createTimeBlocks();
+    //Each time block will be assigned an hour, and from that the color it needs to be.
+    //Add listeners to the buttons that
+        //Get the text from the assosiated textarea
+}
+
+function createTimeBlocks()
+{
+    var timeblockcontainer = $(".container");
+    for (var i = 0; i < 12; i++)
+    {
+        //Creates the row div.
+        var newrow = $("<div class=\"row time-block\"></div>");
+
+        //Creates the time marker on the left of the block.
+        var newhour = $("<div class=\"hour\"></div>");
+        newrow.append(newhour);
+
+        //Creates the textarea.
+        var newtextarea = $("<textarea name=\"textarea\" class=\"flex-grow-1\" id=\"textarea\"></textarea>");
+        newrow.append(newtextarea);
+
+        //Creates the save button.
+        var newsavebutton = $("<button class=\"saveBtn\"><i class=\"fas fa-save\"></i></button>");
+        newrow.append(newsavebutton);
+
+        //Adds the new row to the container.
+        timeblockcontainer.append(newrow);
+    }
 }
 
 //Helper functions
 
 function setDate ()//Sets the current date in the jumbotron.
 {
-    $("#currentDay").text(DateTime.local().toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY));
+    $("#currentDay").text(dt.toLocaleString(dt.DATE_MED_WITH_WEEKDAY));
 }
 
 //Listeners
@@ -21,3 +53,7 @@ function setDate ()//Sets the current date in the jumbotron.
 //Calling functions
 
 initialize();//Get the ball rolling.
+
+//Testing
+
+console.log(currentHour);
