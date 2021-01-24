c88c9fa9001449ec16d544b7dc20d9ffa8d303ac
Conner Martin <connerjm2@gmail.com>
2021-01-23 21:26:39 -0800

    Puts the time fields and colors on the time blocks.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Variables
 
 var dt = luxon.DateTime.local();//The DateTime object that we get from Luxon.
-var currentHour;
+var currentHour, startingtime = 10;
 
 //Main functions
 
@@ -19,17 +19,32 @@ function initialize()//Sets up everything when the application is launched.
 function createTimeBlocks()
 {
     var timeblockcontainer = $(".container");
-    for (var i = 0; i < 12; i++)
+    for (var i = startingtime; i < startingtime + 12; i++)
     {
         //Creates the row div.
         var newrow = $("<div class=\"row time-block\"></div>");
 
+        //Assigns the row a color depending on current time.
+        if (i < currentHour)
+        {
+            newrow.addClass("past");
+        }
+        else if (i === currentHour)
+        {
+            newrow.addClass("present");
+        }
+        else
+        {
+            newrow.addClass("future");
+        }
+
         //Creates the time marker on the left of the block.
-        var newhour = $("<div class=\"hour\"></div>");
+        var newhour = $("<div class=\"hour\"></div>").text(i + ":00");
         newrow.append(newhour);
 
         //Creates the textarea.
         var newtextarea = $("<textarea name=\"textarea\" class=\"flex-grow-1\" id=\"textarea\"></textarea>");
+
         newrow.append(newtextarea);
 
         //Creates the save button.
